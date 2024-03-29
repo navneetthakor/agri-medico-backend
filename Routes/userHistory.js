ebf2d133ca877858f8ed1fbf1c192ffbc2bdb61b
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-29 13:50:51 +0530

    addtoUserHistory route added -> validation left

---
+++ Routes/userHistory.js
@@ -0,0 +1,17 @@
+// to use express 
+const express = require('express');
+const router = express.Router();
+
+// to define validation for body parameters 
+const {body} = require('express-validator');
+
+// importing controllers
+const addToUserHistory = require('../Controllers/userHistory/addToUserHistory.js');
+
+// --------------------------ROUTE:1 to push data in userHistory ----------------------------------------------------------
+router.put('/addToUserHistor',
+[
+    body('user_id', "please provide valid user id").not().isEmpty(),
+    body('disease_id', "please provide valid disease id").not().isEmpty(),
+],
+addToUserHistory);
