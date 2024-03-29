252696ff1511772be516b3df9ee9cb15605ded51
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-29 12:26:57 +0530

    userAuthtokenLogin routes completed + checked

--- Routes/user.js
+++ Routes/user.js
@@ -8,9 +8,13 @@ const {body} = require('express-validator');
 // to upload images 
 const upload = require('../Middelwares/fetchImages.js');
 
+// importing middleware 
+const fetchUser = require('../Middelwares/fetchUser.js');
+
 // importing controllers
 const createUser = require('../Controllers/user/createUser.js');
 const userLogin = require('../Controllers/user/userLogin.js');
+const userAuthtokenLogin = require('../Controllers/user/userAuthtokenLogin.js');
 
 
 // --------------------------ROUTE:1 create user account ----------------------------------------------------------
@@ -33,5 +37,10 @@ router.post('/userlogin',
     body("password", "please do enter your password").not().isEmpty()
 ],
 userLogin);
+
+// --------------------------ROUTE:3 login to accoutn with authtoken ( previous login not require) ----------------------------------------------------------
+router.post('/userAuthtokenLogin', 
+fetchUser , 
+userAuthtokenLogin);
 
 module.exports = router;
