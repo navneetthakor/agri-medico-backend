c4a141ad09a0bf1a5e084d34819b75c87c7cc41e
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-30 10:25:36 +0530

    image reading + request sending + flask not receiving image

--- Routes/flaskfetch.js
+++ Routes/flaskfetch.js
@@ -8,21 +8,30 @@ const upload = require('../Middelwares/fetchImages.js');
 // to communicate with ML model
 const axios = require('axios');
 
+// to read file 
+const fs = require('fs');
+const path = require('path');
+
 // --------------------------ROUTE:1 Fetch disease name from flask server -------------------------------------------------------
 
 router.post('/fetchdiseasename', upload.single('image'),  async (req, res) => {
     try {
         
+        // reading image being saved 
+        const savedImage = fs.readFileSync(path.resolve(req.file.path));
+
         // creating formData object 
         const formData = new FormData();
-        formData.append('image',req.file);
+        formData.append('file',savedImage);
+        // formData.append('fileName', req.file.filename);
+        console.log(savedImage);
 
         // const url = `${process.env.MODEL_URL}/`;
-        const url = 'http://127.0.0.1:5000/classify'
-        const body = {
-            formData
-        }
-        const response = await axios.post(url,body);
+        const url = 'http://127.0.0.1:5000/classify';
+        const headers = {
+                'Content-type': 'multipart/form-data'
+            }
+        const response = await axios.post(url,formData,headers);
         const data = response.data;
         console.log(data)
         return res.status(200).json({ "disease": data, signal: "green" })
